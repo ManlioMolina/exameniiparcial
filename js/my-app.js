d3df9d358a851eab9277fae6df8abdea4d7e324f
ManlioMolina <75648145+ManlioMolina@users.noreply.github.com>
2021-03-22 20:36:35 -0600

    init

---
+++ js/my-app.js
@@ -0,0 +1,48 @@
+// Initialize your app
+var myApp = new Framework7({
+	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: false
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'My App',
+	dialog: {
+		title: 'My App',
+		buttonOk: 'Aceptar',
+  	},
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+  		{
+		path: '/info/',
+    	url: 'info.html',
+    	name: 'info',
+  		},
+  		{
+		path: '/info2/',
+    	url: 'info2.html',
+    	name: 'info2',
+  		},
+  		{
+		path: '/account/',
+    	url: 'account.html',
+    	name: 'account',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
